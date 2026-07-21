c4c4b49a2adf14e9f4956ba331fb84d2b9fd82bf
Yonatan <yonatanbellido9@gmail.com>
2026-07-21 01:43:55 -0500

    pequeño avance

---
+++ ms-descubridor/ms-descubridor/src/main/java/com/libros/ms_descubridor/MsDescubridorApplication.java
@@ -0,0 +1,13 @@
+package com.libros.ms_descubridor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDescubridorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDescubridorApplication.class, args);
+	}
+
+}
---
+++ ms-prestamos/ms-prestamos/src/main/java/com/libros/ms_prestamos/MsPrestamosApplication.java
@@ -0,0 +1,13 @@
+package com.libros.ms_prestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPrestamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPrestamosApplication.class, args);
+	}
+
+}
